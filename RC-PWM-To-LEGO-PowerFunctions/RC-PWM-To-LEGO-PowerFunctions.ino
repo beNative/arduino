b91e32c64e04d95ad9e7484f54ad37b46a19bab4
Tim Sinaeve <tim.sinaeve@gmail.com>
2015-06-23 12:45:22 +0200

    updated default spacing

--- RC-PWM-To-LEGO-PowerFunctions/RC-PWM-To-LEGO-PowerFunctions.ino
+++ RC-PWM-To-LEGO-PowerFunctions/RC-PWM-To-LEGO-PowerFunctions.ino
@@ -72,140 +72,140 @@ PowerFunctions pf2(12, 1); // Arduino pin 12, channel 2
 // simple interrupt service routine
 void calcThrottle()
 {
-	if (PCintPort::pinState)
-	{
-		unThrottleInStart = TCNT1;
-	}
-	else
-	{
-		unThrottleInShared = (TCNT1 - unThrottleInStart) >> 1;
-		bUpdateFlagsShared |= THROTTLE_FLAG;
-	}
+  if (PCintPort::pinState)
+  {
+    unThrottleInStart = TCNT1;
+  }
+  else
+  {
+    unThrottleInShared = (TCNT1 - unThrottleInStart) >> 1;
+    bUpdateFlagsShared |= THROTTLE_FLAG;
+  }
 }
 
 void calcSteering()
 {
-	if (PCintPort::pinState)
-	{
-		unSteeringInStart = TCNT1;
-	}
-	else
-	{
-		unSteeringInShared = (TCNT1 - unSteeringInStart) >> 1;
-		bUpdateFlagsShared |= STEERING_FLAG;
-	}
+  if (PCintPort::pinState)
+  {
+    unSteeringInStart = TCNT1;
+  }
+  else
+  {
+    unSteeringInShared = (TCNT1 - unSteeringInStart) >> 1;
+    bUpdateFlagsShared |= STEERING_FLAG;
+  }
 }
 
 void calcAux()
 {
-	if (PCintPort::pinState)
-	{
-		unAuxInStart = TCNT1;
-	}
-	else
-	{
-		unAuxInShared = (TCNT1 - unAuxInStart) >> 1;
-		bUpdateFlagsShared |= AUX_FLAG;
-	}
+  if (PCintPort::pinState)
+  {
+    unAuxInStart = TCNT1;
+  }
+  else
+  {
+    unAuxInShared = (TCNT1 - unAuxInStart) >> 1;
+    bUpdateFlagsShared |= AUX_FLAG;
+  }
 }
 
 void setup()
 {
-	// sets a standard rate of 50 Hz by setting a frame space of 
-	// 10 * 2000 = 3 Servos + 7 times 2000
-	CRCArduinoFastServos::setFrameSpaceA(SERVO_FRAME_SPACE, 7 * 2000);
-	CRCArduinoFastServos::begin();
+  // sets a standard rate of 50 Hz by setting a frame space of
+  // 10 * 2000 = 3 Servos + 7 times 2000
+  CRCArduinoFastServos::setFrameSpaceA(SERVO_FRAME_SPACE, 7 * 2000);
+  CRCArduinoFastServos::begin();
 
-	// using the PinChangeInt library, attach the interrupts used to read the 
-	//channels
-	PCintPort::attachInterrupt(THROTTLE_IN_PIN, calcThrottle, CHANGE);
-	PCintPort::attachInterrupt(STEERING_IN_PIN, calcSteering, CHANGE);
-	PCintPort::attachInterrupt(AUX_IN_PIN, calcAux, CHANGE);
+  // using the PinChangeInt library, attach the interrupts used to read the
+  //channels
+  PCintPort::attachInterrupt(THROTTLE_IN_PIN, calcThrottle, CHANGE);
+  PCintPort::attachInterrupt(STEERING_IN_PIN, calcSteering, CHANGE);
+  PCintPort::attachInterrupt(AUX_IN_PIN, calcAux, CHANGE);
 }
 
 void loop()
 {
-	// create local variables to hold a local copies of the channel inputs
-	// these are declared static so that thier values will be retained
-	// between calls to loop.
-	static uint16_t unThrottleIn;
-	static uint16_t unSteeringIn;
-	static uint16_t unAuxIn;
-	// local copy of update flags
-	static uint8_t bUpdateFlags;
-	// check shared update flags to see if any channels have a new signal
-	if (bUpdateFlagsShared)
-	{
-		// turn interrupts off quickly while we take local copies of the shared 
-		// variables
-		noInterrupts(); 
-		// take a local copy of which channels were updated in case we need to 
-		// use this in the rest of loop
-		bUpdateFlags = bUpdateFlagsShared;
-		// in the current code, the shared values are always populated
-		// so we could copy them without testing the flags
-		// however in the future this could change, so lets
-		// only copy when the flags tell us we can.
-		if (bUpdateFlags & THROTTLE_FLAG)
-		{
- 			unThrottleIn = unThrottleInShared;
-		}
-		if (bUpdateFlags & STEERING_FLAG)
-		{
-			unSteeringIn = unSteeringInShared;
-		}
-		if (bUpdateFlags & AUX_FLAG)
-		{
-			unAuxIn = unAuxInShared;
-		}
-		// clear shared copy of updated flags as we have already taken the updates
-		// we still have a local copy if we need to use it in bUpdateFlags
-		bUpdateFlagsShared = 0;
-        // we have local copies of the inputs, so now we can turn interrupts 
-        // back on as soon as interrupts are back on, we can no longer use the 
-        // shared copies, the ISRs own these and could update them at any time. 
+  // create local variables to hold a local copies of the channel inputs
+  // these are declared static so that thier values will be retained
+  // between calls to loop.
+  static uint16_t unThrottleIn;
+  static uint16_t unSteeringIn;
+  static uint16_t unAuxIn;
+  // local copy of update flags
+  static uint8_t bUpdateFlags;
+  // check shared update flags to see if any channels have a new signal
+  if (bUpdateFlagsShared)
+  {
+    // turn interrupts off quickly while we take local copies of the shared
+    // variables
+    noInterrupts();
+    // take a local copy of which channels were updated in case we need to
+    // use this in the rest of loop
+    bUpdateFlags = bUpdateFlagsShared;
+    // in the current code, the shared values are always populated
+    // so we could copy them without testing the flags
+    // however in the future this could change, so lets
+    // only copy when the flags tell us we can.
+    if (bUpdateFlags & THROTTLE_FLAG)
+    {
+       unThrottleIn = unThrottleInShared;
+    }
+    if (bUpdateFlags & STEERING_FLAG)
+    {
+      unSteeringIn = unSteeringInShared;
+    }
+    if (bUpdateFlags & AUX_FLAG)
+    {
+      unAuxIn = unAuxInShared;
+    }
+    // clear shared copy of updated flags as we have already taken the updates
+    // we still have a local copy if we need to use it in bUpdateFlags
+    bUpdateFlagsShared = 0;
+        // we have local copies of the inputs, so now we can turn interrupts
+        // back on as soon as interrupts are back on, we can no longer use the
+        // shared copies, the ISRs own these and could update them at any time.
         // During the update, the shared copies may contain junk. Luckily we
         // have our local copies to work with :-)
-		interrupts(); 
-	}
-	bUpdateFlags = 0;
-	int t;
-	if (unThrottleIn < 1500)
-	{
-		t = map(unThrottleIn, 1100, 1500, 9, 16);
-	}
-	else if (unThrottleIn >= 1500)
-	{
-		t = map(unThrottleIn, 1500, 1900, 0, 7);
-	}
-	int s;
-	if (unSteeringIn < 1500)
-	{
-		s = map(unSteeringIn, 1100, 1500, 9, 16);
-	}
-	else if (unSteeringIn >= 1500)
-	{
-		s = map(unSteeringIn, 1500, 1900, 0, 7);
-	}
-	int a;
-	if (unAuxIn < 1500)
-	{
-		a = 0;
-	}
-	else
-	{
-		a = 1;
-	}
-	if ((s != 0) || (t != 0))
-	{
-		// send combined signal for both RED and BLUE channel
-	}
-	if (a == 1)
-	{
-		pf1.combo_pwm(t, s);
-	}
-	else
-	{
-		pf2.combo_pwm(t, s);
-	}
+    interrupts();
+  }
+  bUpdateFlags = 0;
+  int t;
+  if (unThrottleIn < 1500)
+  {
+    t = map(unThrottleIn, 1100, 1500, 9, 16);
+  }
+  else if (unThrottleIn >= 1500)
+  {
+    t = map(unThrottleIn, 1500, 1900, 0, 7);
+  }
+  int s;
+  if (unSteeringIn < 1500)
+  {
+    s = map(unSteeringIn, 1100, 1500, 9, 16);
+  }
+  else if (unSteeringIn >= 1500)
+  {
+    s = map(unSteeringIn, 1500, 1900, 0, 7);
+  }
+  int a;
+  if (unAuxIn < 1500)
+  {
+    a = 0;
+  }
+  else
+  {
+    a = 1;
+  }
+  if ((s != 0) || (t != 0))
+  {
+    // send combined signal for both RED and BLUE channel
+  }
+  if (a == 1)
+  {
+    pf1.combo_pwm(t, s);
+  }
+  else
+  {
+    pf2.combo_pwm(t, s);
+  }
 }
